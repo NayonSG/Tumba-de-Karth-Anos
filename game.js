4d8f2ef68d80ebab9b1f92ff93a2430688efbd75
NayonSG <nayons.gomes@gmail.com>
2022-06-23 16:31:19 -0300

    Commit inicial

---
+++ game.js
@@ -0,0 +1,20 @@
+const textElement = document.getElementById('text')
+const optionBtns = document.getElementById('option-btns')
+
+let state {}
+
+function startGame() {
+    state {}
+    showTextNode (1)
+
+}
+
+function showTextNode(textNodeIndex) {
+
+}
+
+function selectOptiion(option){
+
+}
+
+startGame ()
